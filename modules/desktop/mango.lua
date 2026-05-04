c9bec69ed680fc5cec6a736731037c79781ffb8d
Michael Buckley <michaelcbuckley@proton.me>
2026-05-04 18:44:08 -0400

    Mango: added waybar and changed configs, like disabling animations

--- modules/desktop/mango.lua
+++ modules/desktop/mango.lua
@@ -1,5 +1,11 @@
 local rb = require("rootbeer")
 local dir = "configs/desktop/mango"
+local waybar = "configs/desktop/mango/waybar"
+
 rb.link_file(dir .. "/config.conf", "~/.config/mango/config.conf")
 rb.link_file(dir .. "/binds.conf", "~/.config/mango/binds.conf")
-rb.link_file(dir .. "/noctalia.conf", "~/.config/mango/noctalia.conf")
+--rb.link_file(dir .. "/noctalia.conf", "~/.config/mango/noctalia.conf")
+rb.link_file(dir .. "/waybar.conf", "~/.config/mango/waybar.conf")
+
+rb.link_file(waybar .. "/config.jsonc", "~/.config/mango/waybar/config.jsonc")
+rb.link_file(waybar .. "/style.css", "~/.config/mango/waybar/style.css")
