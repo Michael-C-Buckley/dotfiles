d41449d0728259a1a0ca2b5638ff167783c6b4e0
Michael Buckley <michaelcbuckley@proton.me>
2026-05-07 05:36:34 -0400

    Mango/DMS: added DMS shell as an option

--- modules/desktop/mango.lua
+++ modules/desktop/mango.lua
@@ -6,7 +6,8 @@ local waybar = dir .. "/waybar"
 rb.link_file(dir .. "/config.conf", "~/.config/mango/config.conf")
 rb.link_file(dir .. "/binds.conf", "~/.config/mango/binds.conf")
 --rb.link_file(dir .. "/noctalia.conf", "~/.config/mango/noctalia.conf")
-rb.link_file(dir .. "/waybar.conf", "~/.config/mango/waybar.conf")
+rb.link_file(dir .. "/dms.conf", "~/.config/mango/dms.conf")
+-- rb.link_file(dir .. "/waybar.conf", "~/.config/mango/waybar.conf")
 
 rb.link_file(waybar .. "/config.jsonc", "~/.config/mango/waybar/config.jsonc")
 rb.link_file(waybar .. "/style.css", "~/.config/mango/waybar/style.css")
