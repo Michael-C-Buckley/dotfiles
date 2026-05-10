75dcd3034eb7fd2b91692eb21960ecea0d3b1502
Michael Buckley <michaelcbuckley@proton.me>
2026-05-09 22:05:42 -0400

    Config/Nvim: theme tweaking for customized quiet for tabline

--- configs/editor/nvim/lua/ui/themes.lua
+++ configs/editor/nvim/lua/ui/themes.lua
@@ -1,44 +1,42 @@
--- === Colorscheme ===
 local palette = {
-  main = {
-    ["primary"] = "#DFE0DC",
-    ["primary_dark"] = "#888888",
-    ["accent"] = "#F6C177",
-    ["red"] = "#EB6F92",
-    ["blue"] = "#9CCFD8",
-  },
+  ["primary"] = "#DFE0DC",
+  ["primary_dark"] = "#888888",
+  ["accent"] = "#F6C177",
+  ["red"] = "#EB6F92",
+  ["blue"] = "#9CCFD8",
+  ["dark_tab"] = "#2E2929",
+  ["black"] = "#111111",
+  ["gray1"] = "#6F6262",
 }
 
-function hl(name, set)
+function HL(name, set)
   vim.api.nvim_set_hl(0, name, set)
 end
 
-palette = palette.main
-
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = { "quiet" },
+  pattern = { "quiet" }, -- or a specific scheme name like "gruvbox"
   callback = function()
-    hl("Normal", { fg = palette.primary })
+    HL("Normal", { fg = palette.primary })
 
-    hl("Comment", { fg = palette.primary_dark })
-    hl("@comment", { link = "Comment" })
-    hl("rustCommentLineDoc", { link = "Comment" })
-    hl("LineNr", { link = "Comment" })
-    hl("LineNrAbove", { link = "Comment" })
-    hl("LineNrBelow", { link = "Comment" })
+    HL("Comment", { fg = palette.primary_dark })
+    HL("@comment", { link = "Comment" })
+    HL("rustCommentLineDoc", { link = "Comment" })
+    HL("LineNr", { link = "Comment" })
+    HL("LineNrAbove", { link = "Comment" })
+    HL("LineNrBelow", { link = "Comment" })
 
-    hl("Directory", { fg = palette.accent })
-    hl("String", { fg = palette.accent })
-    hl("@string", { link = "String" })
-    hl("TODO", { fg = palette.red })
-    hl("MatchParen", { fg = palette.red })
+    HL("Directory", { fg = palette.accent })
+    HL("String", { fg = palette.accent })
+    HL("@string", { link = "String" })
+    HL("TODO", { fg = palette.red })
+    HL("MatchParen", { fg = palette.red })
 
-    -- hl("MiniTablineCurrent", { fg = palette.
-    --- - `MiniTablineVisible` - buffer is visible (displayed in some window).
-    --- - `MiniTablineHidden` - buffer is hidden (not displayed).
-    --- - `MiniTablineModifiedCurrent` - buffer is modified and current.
-    --- - `MiniTablineModifiedVisible` - buffer is modified and visible.
-    --- - `MiniTablineModifiedHidden` - buffer is modified and hidden.
+    HL("MiniTablineCurrent", { bg = palette.dark1, fg = palette.accent })
+    HL("MiniTablineHidden", { bg = palette.black, fg = palette.primary })
+    HL("MiniTablineVisible", { bg = palette.black, fg = palette.primary_dark })
+    HL("MiniTablineModifiedCurrent", { bg = palette.primary, fg = palette.black })
+    HL("MiniTablineModifiedVisible", { bg = palette.gray1, fg = palette.primary_dark })
+    HL("MiniTablineModifiedHidden", { bg = palette.blue, fg = palette.black })
 
     vim.api.nvim_set_hl(
       0,
@@ -46,11 +44,11 @@ vim.api.nvim_create_autocmd("ColorScheme", {
       { bg = palette.red, fg = "#000000" }
     )
 
-    hl("Visual", { bg = "#333333" })
-    hl("QuickFixLine", { link = "Visual" })
-    hl("NormalFloat", { bg = "#0A0A0A" })
-    hl("StatusLine", { bg = "#111111" })
-    hl("ColorColumn", { bg = "#222222" })
+    HL("Visual", { bg = "#333333" })
+    HL("QuickFixLine", { link = "Visual" })
+    HL("NormalFloat", { bg = "#0A0A0A" })
+    HL("StatusLine", { bg = "#111111" })
+    HL("ColorColumn", { bg = "#222222" })
 
     vim.api.nvim_set_hl(
       0,
