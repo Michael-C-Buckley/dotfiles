4c7e3d3b1d0de791faf36d36fbd7cdd42c8753c0
Michael Buckley <michaelcbuckley@proton.me>
2026-05-12 23:13:02 -0400

    Config/Helix: migrated helix into rootbeer

---
+++ modules/apps/helix.lua
@@ -0,0 +1,4 @@
+local rb = require("rootbeer")
+rb.link_file("configs/editor/helix/config.toml", "~/.config/helix/config.toml")
+rb.link_file("configs/editor/helix/languages.toml", "~/.config/helix/languages.toml")
+
