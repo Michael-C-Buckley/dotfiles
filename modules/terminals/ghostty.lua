5cad63ca394733f5622430961a02cd6060aa367b
Michael Buckley <michaelcbuckley@proton.me>
2026-05-13 21:38:52 -0400

    Ghostty/Config: split linux config

--- modules/terminals/ghostty.lua
+++ modules/terminals/ghostty.lua
@@ -7,7 +7,6 @@ local config = [[
 cursor-color = #44A3A3
 cursor-opacity = 0.6
 font-family = Cascadia Code NF
-font-size = 14
 background = #000000
 keybind = performable:ctrl+shift+h=previous_tab
 keybind = performable:ctrl+shift+l=next_tab
@@ -15,10 +14,15 @@ keybind = performable:ctrl+shift+l=next_tab
 
 if rb.host.os == "macos" then
   config = config .. [[
+font-size = 14
 font-thicken = true
 font-thicken-strength = 155
 command = /opt/homebrew/bin/fish
-    ]]
+]]
+else
+  config = config .. [[
+font-size = 11
+]]
 end
 
 rb.file("~/.config/ghostty/config", config)
