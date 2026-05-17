034d0a6b012cfb7a627cbe7a177e4358f9652f54
Michael Buckley <michaelcbuckley@proton.me>
2026-05-17 19:54:01 -0400

    T14/Config: added hyprland

--- hosts/t14/host.lua
+++ hosts/t14/host.lua
@@ -1,5 +1,6 @@
 local rb = require("rootbeer")
 require("modules.presets.linux")
+require("modules.desktop.hyprland")
 
--- Mango Host File
 rb.link_file("hosts/t14/mango.conf", "~/.config/mango/host.conf")
+rb.link("hosts/t14/hyprland.lua", "~/.config/hypr/host.lua")
--- hosts/t14g5/host.lua
+++ hosts/t14g5/host.lua
@@ -2,6 +2,5 @@ local rb = require("rootbeer")
 require("modules.presets.linux")
 require("modules.desktop.hyprland")
 
--- Mango Host File
 rb.link_file("hosts/t14g5/mango.conf", "~/.config/mango/host.conf")
 rb.link("hosts/t14g5/hyprland.lua", "~/.config/hypr/host.lua")
