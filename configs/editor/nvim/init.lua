421b49c51b24487816132e959184bc3a5230e7d4
Michael Buckley <michaelcbuckley@proton.me>
2026-05-26 21:14:11 -0400

    Nvim: fixed the python crash by changing the import order

--- configs/editor/nvim/init.lua
+++ configs/editor/nvim/init.lua
@@ -5,8 +5,6 @@ else
   -- Everything else is the normal flow
   require("ui.themes")
   vim.pack.add({ "https://github.com/Shatur/neovim-ayu" })
-  vim.pack.add({ "https://github.com/slugbyte/lackluster.nvim" })
-  require("lackluster")
   require("ayu")
 
   -- Customized and super minimal version of quiet with just a tiny bit of useful highlighting
@@ -16,15 +14,15 @@ else
   vim.diagnostic.config({ virtual_lines = { current_line = true } })
   vim.opt.laststatus = 3
   vim.opt.statusline = "%<%{expand('%:~')}(%(%l:%v%))"
-      .. " %{exists('b:git_branch') ? b:git_branch : ''}"
-      .. " %h%w%{&modified ?  '[MODIFIED]' : ''}%r"
-      .. " %=Rune=%B"
-      .. " Byte_Index=%o"
-      .. " %q"
-      .. " %P"
+    .. " %{exists('b:git_branch') ? b:git_branch : ''}"
+    .. " %h%w%{&modified ?  '[MODIFIED]' : ''}%r"
+    .. " %=Rune=%B"
+    .. " Byte_Index=%o"
+    .. " %q"
+    .. " %P"
 
   require("base")
-  require("languages")
   require("utility")
+  require("languages")
   require("ui.mini")
 end
