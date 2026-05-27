32fe4420051f2f9393f892e7e29974f3886f4ea7
Michael Buckley <michaelcbuckley@proton.me>
2026-05-27 09:18:05 -0400

    Nvim: more themes

--- configs/editor/nvim/lua/ui/themes.lua
+++ configs/editor/nvim/lua/ui/themes.lua
@@ -4,5 +4,9 @@ vim.pack.add({
   "https://github.com/vague-theme/vague.nvim",
   "https://github.com/oskarnurm/koda.nvim",
   "https://github.com/dasupradyumna/midnight.nvim",
-  "https://github.com/Biscuit-Theme/nvim",
+  "https://github.com/navarasu/onedark.nvim",
+  "https://github.com/xero/miasma.nvim",
+  "https://github.com/ribru17/bamboo.nvim",
+  "https://github.com/ellisonleao/gruvbox.nvim",
+  { src = "https://github.com/everviolet/nvim", name = "evergarden" },
 })
